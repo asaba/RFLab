76d8e37d8d06346738dbc69ebc4e5417d227c1b1
Andrea Saba <Andrea Saba>
2016-06-17 10:25:39 +0200

    Change for use of generic Frame

--- PlotAllanDevgui.py
+++ PlotAllanDevgui.py
@@ -5,6 +5,8 @@ Created on 26/dic/2015
 '''
 
 #import images
+from taskframe import TaskFrame
+
 import wx
 import os
 
@@ -34,7 +36,7 @@ class NotebookDemo(wx.Notebook):
         self.AddPage(self.tabTSC, "TSC5115A Phase noise test")
  
 ########################################################################
-class PlotAllanDevFrame(wx.Frame):
+class PlotAllanDevFrame(TaskFrame):
     """
     Frame that holds all other widgets
     """
@@ -42,84 +44,30 @@ class PlotAllanDevFrame(wx.Frame):
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
-        wx.Frame.__init__(self, None, wx.ID_ANY,
+        TaskFrame.__init__(self, NotebookDemo,
                           "Calibrate Cable",
                           size=(800,650)
                           )
         
-        menubar = wx.MenuBar()
-        fileMenu = wx.Menu()
-        self.fitem = fileMenu.Append(wx.ID_OPEN, 'Load settings', 'Load settings')
-        self.fitem2 = fileMenu.Append(wx.ID_SAVEAS, 'Save settings', 'Save settings')
-        self.runmodeitem = fileMenu.AppendCheckItem(7890, "Testing Mode", "Enable testing mode")
-        menubar.Append(fileMenu, '&File')
-        self.SetMenuBar(menubar)
-        
-        self.Bind(wx.EVT_MENU, self.OnLoadSettings, self.fitem)
-        self.Bind(wx.EVT_MENU, self.OnSaveSettings, self.fitem2)
-        #self.Bind(wx.EVT_MENU, self.OnCheckRunMode, self.runmodeitem)
-        
-        self.panel = wx.Panel(self)
-        self.btn_execute = wx.Button(self.panel, 0, 'Start')
-        self.btn_execute.Bind(wx.EVT_BUTTON, self.OnStart)
-        
-        self.notebook = NotebookDemo(self.panel)
-        #notebook2 = NotebookDemo(panel)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.notebook, 1, wx.ALL|wx.EXPAND, 5)
-        #sizer.Add(notebook2, 0, wx.ALL|wx.EXPAND, 5)
-        sizer.Add(self.btn_execute, 0, wx.ALL|wx.EXPAND, 5)
-        self.panel.SetSizer(sizer)
-        self.Layout()
- 
-        self.Show()
-    
-    
-    #def OnCheckRunMode(self, event):
-    #    if self.runmodeitem.IsChecked():
-    #        TEST_MODE = True
-    #        #self.runmodeitem.Check()
-    #    else:
-    #        TEST_MODE = False
-    
-    def OnLoadSettings(self, event):
-        dlg = wx.FileDialog(self, "Choose a file", os.getcwd(), "", "*.cfg", wx.OPEN)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            #mypath = os.path.basename(path)
-            f = open(path, "r")
-            for line in f:
-                parameter = line.split("=")[0].strip()
-                value =  line.split("=")[1].strip()
-                exec("{param}.SetValue({value})".format(param = parameter, value = value))
                         
-    def savesettings(self, filepointer):
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.TSC_state")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.instrument_txt_IP")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.instrument_txt_Port")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.instrument_txt_Timeout")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.TSC_collecting_delay")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.TSC_plot_adev")
-        self.writelinesettings(filepointer, "self.notebook.tabTSC.result_file_name")         
+    def savesettings(self, filename):
+        
+        params = ["tabTSC.TSC_state",
+        "tabTSC.instrument_txt_IP",
+        "tabTSC.instrument_txt_Port",
+        "tabTSC.instrument_txt_Timeout",
+        "tabTSC.combobox_instrtype",
+        "tabTSC.TSC_collecting_delay",
+        "tabTSC.TSC_plot_adev",
+        "tabTSC.result_file_name"]    
+        
+        TaskFrame.framesavesettings(self, filename, params = params)     
     
-    def OnSaveSettings(self, event):
-        dlg = wx.FileDialog(self, "Choose a file", os.getcwd(), "", "*.cfg", wx.SAVE)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            f = open(path, "w")
-            self.savesettings(f)
-            f.close()
-                        
-    def writelinesettings(self, f, parameter):
-        value = None
-        exec("value = {param}.GetValue()".format(param = parameter))
-        writelineonfilesettings(f, parameter, value)
     
     def OnStart(self, event):
                 
-        if self.runmodeitem.IsChecked():
-            dlg = wx.MessageDialog(None, 'Test mode. Instruments comunication disabled',"Test mode",  wx.OK | wx.ICON_ERROR)
-            dlg.ShowModal()
+        TaskFrame.OnStart(self, event)
+        
         #Check all values
         TSC_IP = self.notebook.tabTSC.instrument_txt_IP.GetValue()
         if check_value_is_IP(TSC_IP, "TSC IP") == 0:
@@ -153,16 +101,14 @@ class PlotAllanDevFrame(wx.Frame):
             dlg.ShowModal()
             return 0
         
+        self.savesettings(result_file_name)
+        
         dialog = wx.ProgressDialog("Progress", "Time remaining", maximum = 100,
                 style=wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME | wx.PD_REMAINING_TIME)
         
         TSC_measure(TSC, TSC_collecting_delay, result_file_name, TSC_plot_adev, createprogressdialog = dialog)
  
         dialog.Destroy()
-        filesettingname = result_file_name + "_calcable_" + return_now_postfix() + ".cfg"
-        f = open(filesettingname, "w")
-        self.savesettings(f)
-        f.close()
         
 #----------------------------------------------------------------------
 if __name__ == "__main__":
