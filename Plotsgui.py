81533a3b6d33d974b66af3844c2846f83c174560
Andrea Saba <Andrea Saba>
2016-06-17 18:44:27 +0200

    Added menu for select font style

--- Plotsgui.py
+++ Plotsgui.py
@@ -14,6 +14,7 @@ from utilitygui import check_value_is_valid_file, check_value_min_max, check_val
 from utility import writelineonfilesettings, return_now_postfix,\
     Generic_Range
 from measure_scripts.scriptutility import Frequency_Range
+from measure_scripts.graphutility import font_styles, fontsizerange
 
 
 
@@ -57,7 +58,147 @@ class PlotsFrame(TaskFrame):
                           size=(800,650),
                           start_button = False
                           )
-                        
+        
+        self.setupMenu = wx.Menu()
+        self.superior_title = wx.Menu()
+        self.title = wx.Menu()
+        self.legend_title = wx.Menu()
+        self.legend_lines = wx.Menu()
+        self.axis_legend = wx.Menu()
+        self.axis_ticks = wx.Menu()
+        self.annotation = wx.Menu()
+        
+        self.superior_title_font = wx.Menu()
+        self.superior_title_font_size = wx.Menu()
+        self.superior_title_font_size_mi = []
+        for i in fontsizerange:
+            self.superior_title_font_size_mi.append(self.superior_title_font_size.AppendRadioItem(8800+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.superior_title_font_size_mi[-1])
+        self.superior_title_font.AppendMenu(8700, "Size", self.superior_title_font_size)
+        self.superior_title.AppendMenu(8600, "Font", self.superior_title_font)
+        self.setupMenu.AppendMenu(8500, 'Superior Title', self.superior_title)
+        
+        self.title_font = wx.Menu()
+        self.title_font_size = wx.Menu()
+        self.title_font_size_mi = []
+        for i in fontsizerange:
+            self.title_font_size_mi.append(self.title_font_size.AppendRadioItem(8400+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.title_font_size_mi[-1])
+        self.title_font.AppendMenu(8300, "Size", self.title_font_size)
+        self.title.AppendMenu(8200, "Font", self.title_font)
+        self.setupMenu.AppendMenu(8100, 'Title', self.title)
+        
+        self.legend_title_font = wx.Menu()
+        self.legend_title_font_size = wx.Menu()
+        self.legend_title_font_size_mi = []
+        for i in fontsizerange:
+            self.legend_title_font_size_mi.append(self.legend_title_font_size.AppendRadioItem(8000+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.legend_title_font_size_mi[-1])
+        self.legend_title_font.AppendMenu(7900, "Size", self.legend_title_font_size)
+        self.legend_title.AppendMenu(7800, "Font", self.legend_title_font)
+        self.setupMenu.AppendMenu(7700, 'Legend Title', self.legend_title)
+        
+        
+        self.legend_lines_font = wx.Menu()
+        self.legend_lines_font_size = wx.Menu()
+        self.legend_lines_font_size_mi = []
+        for i in fontsizerange:
+            self.legend_lines_font_size_mi.append(self.legend_lines_font_size.AppendRadioItem(7600+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.legend_lines_font_size_mi[-1])
+        self.legend_lines_font.AppendMenu(7500, "Size", self.legend_lines_font_size)
+        self.legend_lines.AppendMenu(7400, "Font", self.legend_lines_font)
+        self.setupMenu.AppendMenu(7300, 'Legend Lines', self.legend_lines)
+        
+        
+        self.axis_legend_font = wx.Menu()
+        self.axis_legend_font_size = wx.Menu()
+        self.axis_legend_font_size_mi = []
+        for i in fontsizerange:
+            self.axis_legend_font_size_mi.append(self.axis_legend_font_size.AppendRadioItem(7200+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.axis_legend_font_size_mi[-1])
+        self.axis_legend_font.AppendMenu(7100, "Size", self.axis_legend_font_size)
+        self.axis_legend.AppendMenu(7000, "Font", self.axis_legend_font)
+        self.setupMenu.AppendMenu(6900, 'Axis Legend', self.axis_legend)
+        
+        self.axis_ticks_font = wx.Menu()
+        self.axis_ticks_font_size = wx.Menu()
+        self.axis_ticks_font_size_mi = []
+        for i in fontsizerange:
+            self.axis_ticks_font_size_mi.append(self.axis_ticks_font_size.AppendRadioItem(6800+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.axis_ticks_font_size_mi[-1])
+        self.axis_ticks_font.AppendMenu(6700, "Size", self.axis_ticks_font_size)
+        self.axis_ticks.AppendMenu(6600, "Font", self.axis_ticks_font)
+        self.setupMenu.AppendMenu(6500, 'Axis Ticks', self.axis_ticks)
+        
+        self.annotation_font = wx.Menu()
+        self.annotation_font_size = wx.Menu()
+        self.annotation_font_size_mi = []
+        for i in range(5, 20):
+            self.annotation_font_size_mi.append(self.annotation_font_size.AppendRadioItem(6400+i, str(i), str(i)))
+            #self.Bind(wx.EVT_MENU, self.OnCheckSetting, self.annotation_font_size_mi[-1])
+        self.annotation_font.AppendMenu(6300, "Size", self.annotation_font_size)
+        self.annotation.AppendMenu(6200, "Font", self.annotation_font)
+        self.setupMenu.AppendMenu(6100, 'Points Text', self.annotation)
+
+        self.menubar.Append(self.setupMenu, '&Settings')
+        self.SetMenuBar(self.menubar)
+        
+        self.set_selected_settings()
+        #self.Bind(wx.EVT_MENU, self.OnLoadSettings, self.fitem)
+        #self.Bind(wx.EVT_MENU, self.OnSaveSettings, self.fitem2)
+        
+    def _select_setting(self, param_name, menu_item_list):
+        
+        for fontsizestyle in self.menu_item_list:
+            if fontsizestyle.IsChecked():
+                font_styles[param_name]["fontsize"] = fontsizestyle.Text
+                break
+        
+    def get_selected_settings(self):
+        
+        self._select_setting("suptitle", self.superior_title_font_size_mi)
+        
+        self._select_setting("title", self.title_font_size_mi)
+        
+        self._select_setting("legendtitle", self.legend_title_font_size_mi)
+        
+        self._select_setting("legendlines", self.legend_lines_font_size_mi)
+            
+        self._select_setting("axislegend", self.axis_legend_font_size_mi)
+            
+        self._select_setting("axisticks", self.axis_ticks_font_size_mi)
+            
+        self._select_setting("annotation", self.annotation_font_size_mi)
+        
+        return font_styles
+    
+    
+    def _set_setting(self, param_name, menu_item_list, font_styles_tmp = None):
+        for fontsizestyle in menu_item_list:
+            if fontsizestyle.Text == font_styles_tmp[param_name]["fontsize"]:
+                fontsizestyle.Check()
+                break
+    
+    def set_selected_settings(self, font_styles_tmp = None):
+        if font_styles_tmp is None:
+            font_styles_tmp = font_styles
+        
+        self._set_setting("suptitle", self.superior_title_font_size_mi, font_styles_tmp)
+        
+        self._set_setting("title", self.title_font_size_mi, font_styles_tmp)
+                
+        self._set_setting("legendtitle", self.legend_title_font_size_mi, font_styles_tmp)
+        
+        self._set_setting("legendlines", self.legend_lines_font_size_mi, font_styles_tmp)
+        
+        self._set_setting("axislegend", self.axis_legend_font_size_mi, font_styles_tmp)
+        
+        self._set_setting("axisticks", self.axis_ticks_font_size_mi, font_styles_tmp)
+        
+        self._set_setting("annotation", self.annotation_font_size_mi, font_styles_tmp)
+        
+        return font_styles
+    
     def savesettings(self, filename):
         params = ["tabIP1PlotGraph.graph_title",
         "tabIP1PlotGraph.graph_x_label",
