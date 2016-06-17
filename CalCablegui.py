7336b37407bc062790a03de8673dd73f66507707
Andrea Saba <Andrea Saba>
2016-06-17 10:27:14 +0200

    Extended for generation of Dummy cable

--- CalCablegui.py
+++ CalCablegui.py
@@ -5,15 +5,16 @@ Created on 26/dic/2015
 '''
 
 #import images
+from taskframe import TaskFrame
 import wx
 import os
 
 from guitabs import TabPanelFSV, TabPanelPowerMeter, TabPanelSMB, TabPanelSpuriusSetup, TabPanelCalCableSetup, TabPanelSAB
 from measure_scripts.CalibrazioneCavo import unit, measure_calibration_cable, SMB_RF, NRP2, SAB
 from measure_scripts.scriptutility import Frequency_Range
-from utilitygui import check_value_is_valid_file, check_value_min_max, check_value_not_none, resultError, resultOK, check_value_is_IP, create_instrument
+from utilitygui import check_value_is_valid_file, check_value_min_max, check_value_not_none, resultError, resultOK, check_value_is_IP, create_instrument, info_message
 from utility import writelineonfilesettings, return_now_postfix
-
+import webbrowser
 
 
 
@@ -46,7 +47,7 @@ class NotebookDemo(wx.Notebook):
         self.AddPage(self.tabCalCableSetting, "Calibrate Cable")
  
 ########################################################################
-class CalCableFrame(wx.Frame):
+class CalCableFrame(TaskFrame):
     """
     Frame that holds all other widgets
     """
@@ -54,107 +55,54 @@ class CalCableFrame(wx.Frame):
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
-        wx.Frame.__init__(self, None, wx.ID_ANY,
+        TaskFrame.__init__(self, NotebookDemo,
                           "Calibrate Cable",
                           size=(800,650)
                           )
         
-        menubar = wx.MenuBar()
-        fileMenu = wx.Menu()
-        self.fitem = fileMenu.Append(wx.ID_OPEN, 'Load settings', 'Load settings')
-        self.fitem2 = fileMenu.Append(wx.ID_SAVEAS, 'Save settings', 'Save settings')
-        self.runmodeitem = fileMenu.AppendCheckItem(7890, "Testing Mode", "Enable testing mode")
-        menubar.Append(fileMenu, '&File')
-        self.SetMenuBar(menubar)
+    def savesettings(self, filename):
         
-        self.Bind(wx.EVT_MENU, self.OnLoadSettings, self.fitem)
-        self.Bind(wx.EVT_MENU, self.OnSaveSettings, self.fitem2)
-        #self.Bind(wx.EVT_MENU, self.OnCheckRunMode, self.runmodeitem)
+        params = ["tabRF.instrument_enable_status",
+        "tabRF.instrument_txt_IP",
+        "tabRF.instrument_txt_Port",
+        "tabRF.instrument_txt_Timeout",
+        "tabRF.combobox_instrtype",
+        "tabRF.synthetizer_frequency_min",
+        "tabRF.synthetizer_frequency_max",
+        "tabRF.synthetizer_frequency_step",
+        "tabRF.synthetizer_frequency_unit",
+        "tabRF.synthetizer_level_fixed",
+        "tabPowerMeter.instrument_enable_status",
+        "tabPowerMeter.instrument_txt_IP",
+        "tabPowerMeter.instrument_txt_Port",
+        "tabPowerMeter.instrument_txt_Timeout",
+        "tabPowerMeter.combobox_instrtype",
+        "tabPowerMeter.power_meter_make_zero",
+        "tabPowerMeter.power_meter_misure_number",
+        "tabPowerMeter.power_meter_misure_delay",
+        "tabSAB.instrument_enable_status",
+        "tabSAB.instrument_txt_IP",
+        "tabSAB.instrument_txt_Port",
+        "tabSAB.instrument_txt_Timeout",
+        "tabSAB.combobox_instrtype",
+        "tabSAB.SAB_switch01_delay",
+        "tabSAB.SAB_switch02_delay",
+        "tabSAB.SAB_switch03_delay",
+        "tabCalCableSetting.result_file_name", 
+        "tabCalCableSetting.output_level",
+        "tabCalCableSetting.create_dummycable_cb"]
         
-        self.panel = wx.Panel(self)
-        self.btn_execute = wx.Button(self.panel, 0, 'Start')
-        self.btn_execute.Bind(wx.EVT_BUTTON, self.OnStart)
-        
-        self.notebook = NotebookDemo(self.panel)
-        #notebook2 = NotebookDemo(panel)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.notebook, 1, wx.ALL|wx.EXPAND, 5)
-        #sizer.Add(notebook2, 0, wx.ALL|wx.EXPAND, 5)
-        sizer.Add(self.btn_execute, 0, wx.ALL|wx.EXPAND, 5)
-        self.panel.SetSizer(sizer)
-        self.Layout()
- 
-        self.Show()
-    
-    
-    #def OnCheckRunMode(self, event):
-    #    if self.runmodeitem.IsChecked():
-    #        TEST_MODE = True
-    #        #self.runmodeitem.Check()
-    #    else:
-    #        TEST_MODE = False
-    
-    def OnLoadSettings(self, event):
-        dlg = wx.FileDialog(self, "Choose a file", os.getcwd(), "", "*.cfg", wx.OPEN)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            #mypath = os.path.basename(path)
-            f = open(path, "r")
-            for line in f:
-                parameter = line.split("=")[0].strip()
-                value =  line.split("=")[1].strip()
-                exec("{param}.SetValue({value})".format(param = parameter, value = value))
-                        
-    def savesettings(self, filepointer):
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_state")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.instrument_txt_IP")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.instrument_txt_Port")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.instrument_txt_Timeout")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_min_unit")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_min")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_max_unit")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_max")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_step_unit")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_step")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_frequency_unit")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_level_min")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_level_max")
-        #self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_level_step")
-        self.writelinesettings(filepointer, "self.notebook.tabRF.synthetizer_level_fixed")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.instrument_txt_IP")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.instrument_txt_Port")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.instrument_txt_Timeout")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.power_meter_state")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.power_meter_make_zero")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.power_meter_misure_number")
-        self.writelinesettings(filepointer, "self.notebook.tabPowerMeter.power_meter_misure_delay")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.instrument_txt_IP")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.instrument_txt_Port")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.instrument_txt_Timeout")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.SAB_switch01_delay")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.SAB_switch02_delay")
-        self.writelinesettings(filepointer, "self.notebook.tabSAB.SAB_switch03_delay")
-        self.writelinesettings(filepointer, "self.notebook.tabCalCableSetting.result_file_name")         
-    
-    def OnSaveSettings(self, event):
-        dlg = wx.FileDialog(self, "Choose a file", os.getcwd(), "", "*.cfg", wx.SAVE)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            f = open(path, "w")
-            self.savesettings(f)
-            f.close()
-                        
-    def writelinesettings(self, f, parameter):
-        value = None
-        exec("value = {param}.GetValue()".format(param = parameter))
-        writelineonfilesettings(f, parameter, value)
+        TaskFrame.framesavesettings(self, filename, params = params)         
     
     def OnStart(self, event):
                 
-        if self.runmodeitem.IsChecked():
-            dlg = wx.MessageDialog(None, 'Test mode. Instruments comunication disabled', "Test mode",  wx.OK | wx.ICON_ERROR)
-            dlg.ShowModal()
-        #Check all values
+        TaskFrame.OnStart(self, event)
+        
+        create_dummy_cable = self.notebook.tabCalCableSetting.create_dummycable_cb.GetValue()
+        
+        if create_dummy_cable:
+            info_message("Dummy cable creation mode. \n No instrument used.", "Dummy Cable Mode")
+        
         synthetizer_IP = self.notebook.tabRF.instrument_txt_IP.GetValue()
         if check_value_is_IP(synthetizer_IP, "Synthetizer IP") == 0:
             return None
@@ -168,6 +116,8 @@ class CalCableFrame(wx.Frame):
             return None
 
         syntetizer_instrType = self.notebook.tabRF.combobox_instrtype.GetValue()
+        
+        syntetizer_enable_state = self.notebook.tabRF.instrument_enable_status.GetValue()
         
         #synthetizer_frequency_min_unit = unit.return_unit(self.notebook.tabRF.synthetizer_frequency_min_unit.GetValue())
         #if check_value_not_none(synthetizer_frequency_min_unit, "Minimum Frequency Unit") == 0:
@@ -217,6 +167,7 @@ class CalCableFrame(wx.Frame):
             
         power_meter_instrType = self.notebook.tabPowerMeter.combobox_instrtype.GetValue()
     
+        power_meter_enable_state = self.notebook.tabPowerMeter.instrument_enable_status.GetValue()
 
         power_meter_make_zero = self.notebook.tabPowerMeter.power_meter_make_zero.GetValue()
         power_meter_make_zero_delay = self.notebook.tabPowerMeter.power_meter_misure_delay.GetValue()
@@ -246,6 +197,8 @@ class CalCableFrame(wx.Frame):
         if check_value_min_max(SAB_Timeout, "SwitchAttBox Timeout", minimum = 0) == 0:
             return None
         
+        SAB_enable_state = self.notebook.tabSAB.instrument_enable_status.GetValue()
+        
         SAB_instrType = self.notebook.tabSAB.combobox_instrtype.GetValue()
         
         SAB_switch01_delay = self.notebook.tabSAB.SAB_switch01_delay.GetValue()
@@ -260,27 +213,35 @@ class CalCableFrame(wx.Frame):
         else:
             SAB_switch02_delay = eval(self.notebook.tabSAB.SAB_switch02_delay.GetValue())
         result_file_name = self.notebook.tabCalCableSetting.result_file_name.GetValue()
+        dummy_cable_power_level = self.notebook.tabCalCableSetting.output_level.GetValue()
+        
+        if create_dummy_cable:
+            if check_value_not_none(dummy_cable_power_level, "Power Level (Dummy Cable)") == 0:
+                return None
 
         try:
-            SMB_RF = create_instrument(synthetizer_IP, synthetizer_Port, eval(synthetizer_Timeout), syntetizer_instrType, TEST_MODE = self.runmodeitem.IsChecked())
+            SMB_RF = create_instrument(synthetizer_IP, synthetizer_Port, eval(synthetizer_Timeout), syntetizer_instrType, TEST_MODE = self.runmodeitem.IsChecked(), enable_state = syntetizer_enable_state and not create_dummy_cable)
         except:
             dlg = wx.MessageDialog(None, "Synthetizer comunication error", 'Error Synthetizer', wx.OK | wx.ICON_ERROR)
             dlg.ShowModal()
             return 0
         
         try:
-            NRP2 = create_instrument(power_meter_IP, power_meter_Port, eval(power_meter_Timeout), power_meter_instrType, TEST_MODE = self.runmodeitem.IsChecked(), instrument_class = "NRP2")
+            NRP2 = create_instrument(power_meter_IP, power_meter_Port, eval(power_meter_Timeout), power_meter_instrType, TEST_MODE = self.runmodeitem.IsChecked(), instrument_class = "NRP2", enable_state = power_meter_enable_state and not create_dummy_cable)
         except:
             dlg = wx.MessageDialog(None, "Power meter comunication error", 'Error Power meter', wx.OK | wx.ICON_ERROR)
             dlg.ShowModal()
             return 0
         
         try:
-            SAB = create_instrument(SAB_IP, SAB_Port, eval(SAB_Timeout), SAB_instrType, TEST_MODE = self.runmodeitem.IsChecked(), instrument_class = "SAB")
+            SAB = create_instrument(SAB_IP, SAB_Port, eval(SAB_Timeout), SAB_instrType, TEST_MODE = self.runmodeitem.IsChecked(), instrument_class = "SAB", enable_state = SAB_enable_state and not create_dummy_cable)
         except:
             dlg = wx.MessageDialog(None, "SwitchAttBox comunication error", 'Error SwitchAttBox', wx.OK | wx.ICON_ERROR)
             dlg.ShowModal()
             return 0
+        
+          
+        self.savesettings(result_file_name)
         
         dialog = wx.ProgressDialog("Progress", "Time remaining", maximum = 100,
                 style=wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME | wx.PD_REMAINING_TIME)
@@ -288,7 +249,7 @@ class CalCableFrame(wx.Frame):
         synthetizer_frequency = Frequency_Range(synthetizer_frequency_min, synthetizer_frequency_max, synthetizer_frequency_step, synthetizer_frequency_unit)
         synthetizer_frequency.to_base()
         
-        measure_calibration_cable(SMB_RF, 
+        calibration_file_result = measure_calibration_cable(SMB_RF, 
                                   NRP2, 
                                   SAB, 
                                   synthetizer_frequency, 
@@ -300,13 +261,16 @@ class CalCableFrame(wx.Frame):
                                   SAB_switch01_delay, 
                                   SAB_switch02_delay, 
                                   result_file_name, 
+                                  dummy_cable_power_level,
+                                  create_dummy_cable,
                                   createprogressdialog = dialog)
  
         dialog.Destroy()
-        filesettingname = result_file_name + "_calcable_" + return_now_postfix() + ".cfg"
-        f = open(filesettingname, "w")
-        self.savesettings(f)
-        f.close()
+        
+        try:
+            webbrowser.open(os.path.dirname(calibration_file_result))
+        except:
+            pass
         
 #----------------------------------------------------------------------
 if __name__ == "__main__":
